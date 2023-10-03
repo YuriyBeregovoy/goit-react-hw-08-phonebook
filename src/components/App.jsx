df978f3375a7229410921e0a5c90b0ea4af2d2a1
YuriyBeregovoy <rik0681@gmail.com>
2023-10-03 11:00:30 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,17 @@ import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { Layout } from "./Layout";
-import {  useEffect } from 'react';
+import {  useEffect, lazy } from 'react';
 import { fetchContacts } from 'redux/contacts/operations';
 import { getIsLoading, getError } from "redux/contacts/contactsSlice";
 import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+// import { useAuth } from 'hooks';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const ContactsPage = lazy(() => import('../pages/Tasks'));
 
 export const App = () => {
   const dispatch = useDispatch();
