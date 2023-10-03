f0ff01fb9ea1428e6e107ff910a9dd6b754b5833
YuriyBeregovoy <rik0681@gmail.com>
2023-10-03 11:46:16 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,40 @@
-import styled from "styled-components";
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AppBar } from './AppBar/AppBar';
+import { Suspense } from 'react';
+
+export const Layout = () => {
+  return (
+    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+      <AppBar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+      <Toaster position="top-right" reverseOrder={false} />
+    </div>
+  );
+};
 
 
-export const Layout = styled.div`
 
-  width: 100%;
-  max-width: 428px;
-  padding-left: 15px;
-  padding-right: 15px;
-  margin-right: auto;
-  margin-left: auto;
- display: flex;
-  flex-direction:column;
-  align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-`;
+
+
+
+
+// import styled from "styled-components";
+
+
+// export const Layout = styled.div`
+
+//   width: 100%;
+//   max-width: 428px;
+//   padding-left: 15px;
+//   padding-right: 15px;
+//   margin-right: auto;
+//   margin-left: auto;
+//  display: flex;
+//   flex-direction:column;
+//   align-items: center;
+//   padding-top: 40px;
+//   padding-bottom: 40px;
+// `;
