8f39a246282081993652e3a9ff8a992578304e39
YuriyBeregovoy <rik0681@gmail.com>
2023-10-04 18:29:57 +0300

    Update authoperations.js

--- src/redux/auth/authoperations.js
+++ src/redux/auth/authoperations.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
