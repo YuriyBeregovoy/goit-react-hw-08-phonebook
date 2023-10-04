62ac97ad719fe8dd5e6190bfd9ec521ddb2529be
YuriyBeregovoy <rik0681@gmail.com>
2023-10-04 18:00:04 +0300

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,7 +10,7 @@ export const Navigation = () => {
         Home
       </NavLink>
       {isLoggedIn && (
-        <NavLink  to="/tasks">
+        <NavLink  to="/contacts">
           Tasks
         </NavLink>
       )}
