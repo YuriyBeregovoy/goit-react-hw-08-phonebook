cf58fb92198912f42e03a8d11d2115a0cac98da5
YuriyBeregovoy <rik0681@gmail.com>
2023-10-04 20:20:05 +0300

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { ContactForm } from "../components/ContactForm/ContactForm";
 import { ContactList } from "../components/ContactList/ContactList";
 import { Filter } from "../components/Filter/Filter";
-import { Layout } from "../components/Layout";
 import {  useEffect } from 'react';
 import { fetchContacts } from '../redux/contacts/operations';
 import { getIsLoading, getError } from "../redux/contacts/contactsSlice";
@@ -18,14 +17,14 @@ export  default function Contacts () {
   }, [dispatch]);
 
     return (
-    <Layout>
+    <>
       <h1>Phonebook</h1>
         <ContactForm />
         {isLoading && !error ? <b>Request in progress...</b> : null}
       <h2>Contacts</h2>
         <Filter />
         <ContactList/>
-    </Layout>
+    </>
     );
 
 
