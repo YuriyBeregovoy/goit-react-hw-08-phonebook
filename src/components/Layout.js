3876c02f4f8acc215d8a091042a2a4435f8a9032
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 11:05:51 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,21 +20,3 @@ export const Layout = () => {
 
 
 
-
-// import styled from "styled-components";
-
-
-// export const Layout = styled.div`
-
-//   width: 100%;
-//   max-width: 428px;
-//   padding-left: 15px;
-//   padding-right: 15px;
-//   margin-right: auto;
-//   margin-left: auto;
-//  display: flex;
-//   flex-direction:column;
-//   align-items: center;
-//   padding-top: 40px;
-//   padding-bottom: 40px;
-// `;
