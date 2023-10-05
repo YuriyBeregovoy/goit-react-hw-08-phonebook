6484ce20cc78c23f5e7efec6b2c60e3868c47303
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 13:12:32 +0300

    Update UserMenu.styled.js

--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -6,3 +6,32 @@ export const UserMenuStyles = styled.div`
   gap: 5px;
   flex-direction: column;
 `;
+
+export const UserMenuButton = styled.button`
+
+    max-width: 169px;
+    padding: 4px 10px;
+    font-family: inherit;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 1.2;
+    text-align: center;
+    letter-spacing: 0.04em;
+    color: #434455;
+    border: 1px solid gray;
+    border-radius: 4px;
+    background-color: white;
+
+    &:hover, &:focus 
+    {
+    color: #dbcd0b;
+    box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.1), 0px 2px 1px rgba(0, 0, 0, 0.08),
+    0px 2px 2px rgba(0, 0, 0, 0.12);}
+
+
+`;
+
+export const NavStyles = styled.nav`
+display: flex;
+gap: 20px;
+`;
