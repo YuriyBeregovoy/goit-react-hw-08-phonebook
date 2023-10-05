31e86a0fd2552db9aa07659259f2653b141bf34f
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 13:22:11 +0300

    Create AllForm.styled.js

---
+++ src/components/AllForm.styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const FormStyles = styled.form`
+display: flex;
+flex-direction: column;
+gap: 10px;
+min-width: 260px;
+margin-bottom: 20px;
+`;
+
+export const LabelStyles = styled.label`
+display: flex;
+flex-direction: column;
+gap: 5px;
+margin-bottom: 10px;
+`;
+
+export const FormButtonStyles = styled.button`
+
+    display: block;
+    max-width: 169px;
+    align-self: center;
+    padding: 5px 10px;
+    font-family: inherit;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 1.2;
+    text-align: center;
+    letter-spacing: 0.04em;
+    color: inherit;
+    border: 1px solid gray;
+    border-radius: 4px;
+    background-color: white;
+
+    &:hover, &:focus 
+    {
+    color: #4d5ae5;
+    box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.1), 0px 2px 1px rgba(0, 0, 0, 0.08),
+    0px 2px 2px rgba(0, 0, 0, 0.12);}
+
+
+`;
