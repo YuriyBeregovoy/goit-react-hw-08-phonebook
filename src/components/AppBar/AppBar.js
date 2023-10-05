c8e00782e0c92d6cff2338f363f5ffad807db366
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 11:50:44 +0300

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,15 +2,14 @@ import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks/useAuth';
-import { HeaderStyles } from 'components/Container.styled';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <HeaderStyles>
+    <div>
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </HeaderStyles>
+    </div>
   );
 };
