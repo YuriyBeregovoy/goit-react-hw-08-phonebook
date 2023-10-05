bc26045cb93df18afaf34c2825c93c86c5ac00f1
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 11:50:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
          
-            <App />
-           <GlobalStyle />
+          <App />
+          <GlobalStyle />
           
         </PersistGate>
       </Provider>
