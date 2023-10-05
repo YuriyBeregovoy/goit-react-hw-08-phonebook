919fcd637f28ee7bf87d74c311c8d66c24462936
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 11:05:43 +0300

    Create Container.styled.js

---
+++ src/components/Container.styled.js
@@ -0,0 +1,18 @@
+
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+
+  width: 100%;
+  max-width: 428px;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-right: auto;
+  margin-left: auto;
+ display: flex;
+  flex-direction:column;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+`;
