6fc890b35e88348832906734e1c71881803c1f6f
YuriyBeregovoy <rik0681@gmail.com>
2023-10-05 11:05:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Container } from 'components/Container.styled';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <App />
-         <GlobalStyle />
+          <Container>
+            <App />
+           <GlobalStyle />
+          </Container>
         </PersistGate>
       </Provider>
     </BrowserRouter>
