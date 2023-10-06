4d7b0c69de01c83a9bf06f9d418346777d22db3a
YuriyBeregovoy <rik0681@gmail.com>
2023-10-06 16:45:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Layout } from './Layout';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
 import { refreshUser } from 'redux/auth/authoperations';
-import { useAuth } from 'hooks';
+import { useAuth } from '../hooks/useAuth';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
